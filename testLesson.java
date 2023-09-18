e155bde656dda0ae34c1646f1ed206f103e464ef
transfons <145350092+transfons@users.noreply.github.com>
2023-09-18 15:08:49 +0300

    Update testTask.java

--- testLesson.java
+++ testLesson.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Kalk {
+class Calc {
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
@@ -18,20 +18,20 @@ class Kalk {
         String[] operands = expression.split("[+\\-*/]");
         if (operands.length != 2) throw new Exception("Должно быть два операнда");
         oper = detectOperation(expression);
-        //если оба числа римские
+        // Если оба числа римские
         if (Roman.isRoman(operands[0]) && Roman.isRoman(operands[1])) {
-            //конвертируем оба числа в арабские для вычисления действия
+            // Конвертируем оба числа в арабские для вычисления действия
             num1 = Roman.convertToArabian(operands[0]);
             num2 = Roman.convertToArabian(operands[1]);
             isRoman = true;
         }
-        //если оба числа арабские
+        // Если оба числа арабские
         else if (!Roman.isRoman(operands[0]) && !Roman.isRoman(operands[1])) {
             num1 = Integer.parseInt(operands[0]);
             num2 = Integer.parseInt(operands[1]);
             isRoman = false;
         }
-        //если одни число римское, а другое - арабское
+        // Если одни число римское, а другое - арабское
         else {
             throw new Exception("Числа должны быть в одном формате");
         }
@@ -40,17 +40,17 @@ class Kalk {
         }
         int arabian = calc(num1, num2, oper);
         if (isRoman) {
-            //если римское число получилось меньше либо равно нулю, генерируем ошибку
+            // Если римское число получилось меньше, либо равно нулю - генерируем ошибку
             if (arabian <= 0) {
                 throw new Exception("Римское число должно быть больше нуля");
             }
-            //конвертируем результат операции из арабского в римское
+            // Конвертируем результат операции из арабского в римское
             result = Roman.convertToRoman(arabian);
         } else {
-            //Конвертируем арабское число в тип String
+            // Конвертируем арабское число в тип String
             result = String.valueOf(arabian);
         }
-        //возвращаем результат
+        // Возвращаем результат
         return result;
     }
 
